d0bbf03eedfb3286724f8800919c17d264fd1aeb
Christian Alares <christian.alares@gmail.com>
2025-03-12 18:15:27 +0100

    edit base url for auth

--- apps/app/src/lib/auth-client.ts
+++ apps/app/src/lib/auth-client.ts
@@ -1,7 +1,15 @@
 import { createAuthClient } from 'better-auth/react'
 
+const getBaseUrl = () => {
+	if (process.env.VERCEL_URL) {
+		return `https://${process.env.VERCEL_URL}`
+	}
+
+	return 'http://localhost:3000'
+}
+
 type AuthClient = ReturnType<typeof createAuthClient>
 
 export const authClient: AuthClient = createAuthClient({
-	baseURL: 'http://localhost:3000',
+	baseURL: getBaseUrl(),
 })
