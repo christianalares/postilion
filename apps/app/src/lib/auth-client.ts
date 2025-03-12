81403cbc5e9634cd4fe4746f270629607fee233e
Christian Alares <christian.alares@gmail.com>
2025-03-12 18:25:17 +0100

    update better auth config

--- apps/app/src/lib/auth-client.ts
+++ apps/app/src/lib/auth-client.ts
@@ -1,15 +1,5 @@
 import { createAuthClient } from 'better-auth/react'
 
-const getBaseUrl = () => {
-	if (process.env.VERCEL_URL) {
-		return `https://${process.env.VERCEL_URL}`
-	}
-
-	return 'http://localhost:3000'
-}
-
 type AuthClient = ReturnType<typeof createAuthClient>
 
-export const authClient: AuthClient = createAuthClient({
-	baseURL: getBaseUrl(),
-})
+export const authClient: AuthClient = createAuthClient()
