b7373f9b6b82c434dc47f049c9820aa89292791e
Christian Alares <christian.alares@gmail.com>
2025-03-14 19:21:17 +0100

    add force-dynamic to trpc route

--- apps/app/src/app/(app)/settings/team/page.tsx
+++ apps/app/src/app/(app)/settings/team/page.tsx
@@ -1,5 +1,7 @@
 import { TeamNameForm } from '@/components/settings/team-name-form'
 
+export const dynamic = 'force-dynamic'
+
 const TeamPage = () => {
   return (
     <div className="space-y-8">
