50393de936b79bd0fd06f0e24a951da85578d240
Christian Alares <christian.alares@gmail.com>
2025-03-16 21:45:55 +0100

    fix type error

--- apps/app/src/trpc/server.ts
+++ apps/app/src/trpc/server.ts
@@ -10,4 +10,10 @@ export const getQueryClient = cache(makeQueryClient)
 
 const caller = createCallerFactory(appRouter)(createTRPCContext)
 
-export const { trpc, HydrateClient } = createHydrationHelpers<typeof appRouter>(caller, getQueryClient)
+export const {
+  trpc,
+  HydrateClient,
+}: {
+  trpc: typeof caller
+  HydrateClient: React.ComponentType<{ children: React.ReactNode }>
+} = createHydrationHelpers<typeof appRouter>(caller, getQueryClient)
