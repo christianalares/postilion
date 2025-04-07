676b7d239323a2fe08e1637b22c8600860cb2cd0
Christian Alares <christian.alares@gmail.com>
2025-04-07 10:05:57 +0200

    fix auth flow

--- apps/app/src/lib/auth/auth.ts
+++ apps/app/src/lib/auth/auth.ts
@@ -7,6 +7,8 @@ import { cookies } from 'next/headers'
 import { createDefaultTeam, handleInvitedUser } from './utils'
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL!,
+  secret: process.env.BETTER_AUTH_SECRET!,
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
